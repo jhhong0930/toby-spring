e53db9d773f15fdb8b73503ccd343ae38868e9ff
jhhong0930 <jhhong0930@gmail.com>
2022-02-12 13:06:44 +0900

    1.3.2 인터페이스의 도입

---
+++ src/main/java/spring/user/dao/BConnectionMaker.java
@@ -0,0 +1,19 @@
+package spring.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BConnectionMaker implements ConnectionMaker {
+
+    @Override
+    public Connection makeConnection() throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby",
+                "root",
+                "qwe123!@#");
+    }
+}
--- src/main/java/spring/user/dao/UserDao.java
+++ src/main/java/spring/user/dao/UserDao.java
@@ -1,23 +1,21 @@
 package spring.user.dao;
 
-import lombok.RequiredArgsConstructor;
 import spring.user.domain.User;
 
 import java.sql.*;
 
-@RequiredArgsConstructor
 public class UserDao {
 
-    /**
-     * 클래스를 분리함으로써 서로 관심사가 다르고 변화의 성격이 다른 구 코드를 완벽히 분리해냈다
-     * 하지만, UserDao가 SimpleConnectionMaker에 종속적이므로 코드의 수정없이 서브 클래스에 커넥션 생성 기능을 변경할 수 없다
-     * UserDao는 커넥션을 가져오는 구체적인 방법에 종속되어 버린다
-     */
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
+
+    // 인터페이스로 분리하였지만 여전히 UserDao는 인터페이스와 구체적인 클래스를 알아야한다
+    public UserDao() {
+        connectionMaker = new AConnectionMaker();
+    }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        Connection conn = simpleConnectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
 
         PreparedStatement ps = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
         ps.setString(1, user.getId());
@@ -32,7 +30,7 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        Connection conn = simpleConnectionMaker.makeNewConnection();
+        Connection conn = connectionMaker.makeConnection();
 
         PreparedStatement ps = conn.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);

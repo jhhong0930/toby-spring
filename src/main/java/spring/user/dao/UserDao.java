086cc32e0fa499a5882515452338be38e2318fd0
jhhong0930 <jhhong0930@gmail.com>
2022-02-20 00:59:21 +0900

    1.6.1 싱글톤 레지스트리로서의 애플리케이션 컨텍스트

--- src/main/java/spring/user/dao/UserDao.java
+++ src/main/java/spring/user/dao/UserDao.java
@@ -10,6 +10,22 @@ public class UserDao {
 
     private final ConnectionMaker connectionMaker;
 
+    /**
+     * private 생성자를 갖고있기 떄문에 상속 불가
+     * 테스트하기 힘든 싱글톤
+     * 서버환경에서는 싱글톤이 하나만 만들어지는 것을 보장하지 못함
+     */
+//    private static UserDao INSTANCE;
+//
+//    private UserDao(ConnectionMaker connectionMaker) {
+//        this.connectionMaker = connectionMaker;
+//    }
+//
+//    private static synchronized UserDao getInstance() {
+//        if (INSTANCE == null) INSTANCE = new UserDao(???);
+//        return INSTANCE;
+//    }
+
     public void add(User user) throws ClassNotFoundException, SQLException {
 
         Connection conn = connectionMaker.makeConnection();
